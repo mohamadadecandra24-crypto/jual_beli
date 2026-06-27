36ff1a99eb1be28ea1a59f8d4f31c23b664ae25a
Mohamad Ade Candra <mohamadadecandra24@gmail.com>
2026-06-28 01:57:30 +0700

    Fix bootstrap cache path for Vercel

--- api/index.php
+++ api/index.php
@@ -2,6 +2,11 @@
 require __DIR__ . '/../vendor/autoload.php';
 $app = require __DIR__ . '/../bootstrap/app.php';
 $app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp');
+
+if (!file_exists('/tmp/bootstrap/cache')) {
+    mkdir('/tmp/bootstrap/cache', 0777, true);
+}
+$app->useBootstrapPath('/tmp/bootstrap');
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle($request = Illuminate\Http\Request::capture());
 $response->send();
